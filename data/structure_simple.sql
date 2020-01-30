73192e9c8cfffa90510945ac59d1bc028be7539c
JBinggi <juerg.binggeli@gmail.com>
2020-01-30 23:47:13 +0100

    type fix

--- data/structure_simple.sql
+++ data/structure_simple.sql
@@ -2,7 +2,7 @@ ALTER TABLE `contact` ADD `description` TEXT NOT NULL DEFAULT '' AFTER `label`,
 ADD `firstname` VARCHAR(255) NOT NULL DEFAULT '' AFTER `description`,
 ADD `lastname` VARCHAR(255) NOT NULL DEFAULT '' AFTER `firstname`,
 ADD `email_addr` VARCHAR(255) NOT NULL DEFAULT '' AFTER `lastname`,
-ADD `phone` VARCHAR(255) NOT NULL DEFAULT '' AFTER `email_addr`,
+ADD `phone` VARCHAR(20) NOT NULL DEFAULT '' AFTER `email_addr`,
 ADD `featured_image` VARCHAR(255) NOT NULL DEFAULT '' AFTER `phone`,
 ADD `salution_idfs` INT(11) NOT NULL DEFAULT '0' AFTER `featured_image`,
 ADD `title_idfs` INT(11) NOT NULL DEFAULT '0' AFTER `salution_idfs`;
